bb40c75198a43d822e1b14fcf43f62b958b0cd28
Steven Alexander <stevena@kainos.com>
2017-10-26 13:36:01 +0100

    updated server

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
 const Koa = require('koa')
 const app = module.exports = new Koa()
 
-app.use(async ctx => {
-  ctx.body = 'Hello World'
-})
+app.use(async ctx => { ctx.body = 'Hello World' })
 
 if (!module.parent) app.listen(process.env.PORT || 3000)
